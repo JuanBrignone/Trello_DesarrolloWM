e647944b7bd959c75286f11f4fd6d103ec23fbc8
JuanBrignone <juan.brignone@correo.ucu.edu.uy>
2024-08-26 14:40:27 -0300

    añadir tarjetas a ToDo.

---
+++ app.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    console.log("todoList initial state:", document.getElementById('todoList').innerHTML);
+    initializeApp();
+});
+
+function initializeApp(){
+    const addTaskButton = document.getElementById('addTaskButton');
+    const saveTaskButton = document.getElementById('saveTaskButton');
+    const closeModalButtons = document.querySelectorAll('.delete, .modal-card-foot .button');
+
+    console.log("save button founded")
+
+    addTaskButton.addEventListener('click', openTaskModal);
+        closeModalButtons.forEach(button => button.addEventListener('click', closeTaskModal));
+        saveTaskButton.addEventListener('click', saveTask);
+}
+
+function openTaskModal(){
+    const taskModal = document.getElementById('taskModal');
+    taskModal.classList.add('is-active');
+}
+
+function closeTaskModal(){
+    const taskModal = document.getElementById('taskModal');
+    clearModalFields();
+    taskModal.classList.remove('is-active');
+}
+
+function saveTask(){
+    console.log("saveTask funcionando")
+    const taskTitle = document.getElementById('taskTitle').value.trim();
+    const taskDescription = document.getElementById('taskDescription').value.trim();
+
+    if (taskTitle !== '') {
+        addTaskTodoList(taskTitle, taskDescription);
+        closeTaskModal();
+    }
+}
+
+function addTaskTodoList(title, description){
+    console.log("Añadiendo task:", title, description);
+    const todoList = document.getElementById('todoList');
+
+    const taskItem = document.createElement('div');
+    taskItem.className = 'box box-add';
+    taskItem.innerHTML = `
+    <h3 class="title is-6">${title}</h3>
+    <p>${description}</p>
+    `;
+
+todoList.appendChild(taskItem);
+}
+
+function clearModalFields() {
+    document.getElementById('taskTitle').value = '';
+    document.getElementById('taskDescription').value = '';
+}
